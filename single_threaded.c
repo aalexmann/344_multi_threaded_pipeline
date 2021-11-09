0f26de21676f7a58bf518414b825683336d001ad
aalexmann <55524595+aalexmann@users.noreply.github.com>
2021-11-09 03:04:24 -0800

    buffers 1, 2, and 3 initialized and commented

--- single_threaded.c
+++ single_threaded.c
@@ -32,15 +32,41 @@ A program with a pipeline of 4 threads that interact with each other as producer
 // Number of lines that will be produced. This number is less than the size of the buffer. Hence, we can model the buffer as being unbounded.
 #define MAX_LINES 50
 
-// Buffer 1, shared resource between input thread and square-root thread
+// Buffer 1, shared resource between input thread and line separator thread
 int buffer_1[MAX_CHAR];
 // Number of items in the buffer
 int count_1 = 0;
 // Index where the input thread will put the next item
-int prod_idx_1 = 0;
-// Index where the square-root thread will pick up the next item
-int con_idx_1 = 0;
+int produced_idx_1 = 0;
+// Index where the line separator thread will pick up the next item
+int consumed_idx_1 = 0;
 // Initialize the mutex for buffer 1
 pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
 // Initialize the condition variable for buffer 1
-pthread_cond_t full_1 = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty_1 = PTHREAD_COND_INITIALIZER;
+
+// Buffer 2, shared resource between line separator thread and plus sign thread
+int buffer_2[MAX_CHAR];
+// Number of items in the buffer
+int count_2 = 0;
+// Index where the plus sign thread will put the next item
+int produced_idx_2 = 0;
+// Index where the plus sign thread will pick up the next item
+int consumed_idx_2 = 0;
+// Initialize the mutex for buffer 2
+pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
+// Initialize the condition variable for buffer 2
+pthread_cond_t empty_2 = PTHREAD_COND_INITIALIZER;
+
+// Buffer 3, shared resource between plus sign thread and output thread
+int buffer_3[MAX_CHAR];
+// Number of items in the buffer
+int count_3 = 0;
+// Index where the plus sign thread will put the next item
+int produced_idx_3 = 0;
+// Index where the plus sign thread will pick up the next item
+int consumed_idx_3 = 0;
+// Initialize the mutex for buffer 2
+pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
+// Initialize the condition variable for buffer 2
+pthread_cond_t empty_3 = PTHREAD_COND_INITIALIZER;
