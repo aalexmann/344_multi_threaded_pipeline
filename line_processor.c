f98e17943cf3b95f2a9efeb808a9ff8542aef37b
Alex Hidalgo <55524595+aalexmann@users.noreply.github.com>
2021-11-13 15:05:52 -0800

    Cleaned up code, works perfect from keyboard, stdin from files has bug(s) still

--- line_processor.c
+++ line_processor.c
@@ -30,78 +30,81 @@
 
 */
 
-
-// Size of the buffers
+// Global variables:
 #define MAX_CHAR 1000
-// Number of lines that will be produced. This number is less than the size of the buffer. Hence, we can model the buffer as being unbounded.
 #define MAX_LINES 50
 
+// Start and end flags:
 int keep_processing = 1;
 int terminate = 0;
 
+//// Buffers:
+// Flag:
 int buff_1_closed = 0;
-// Buffer 1, shared resource between input thread and line separator thread
-char buffer_1[(MAX_CHAR*MAX_LINES)];
-// Number of items in the buffer
+// Buffer 1, shared resource between input thread and line separator thread:
+char buffer_1[50000];
+// Number of items in the buffer:
 int count_1 = 0;
-// Index where the input thread will put the next item
+// Index where the input thread will put the next item:
 int produced_idx_1 = 0;
-// Index where the line separator thread will pick up the next item
+// Index where the line separator thread will pick up the next item:
 int consumed_idx_1 = 0;
-// Initialize the mutex for buffer 1
+// Initialize the mutex for buffer 1:
 pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
-// Initialize the condition variable for buffer 1
+// Initialize the condition variable for buffer 1:
 pthread_cond_t not_empty_1 = PTHREAD_COND_INITIALIZER;
 
+// Flag:
 int buff_2_closed = 0;
-// Buffer 2, shared resource between line separator thread and plus sign thread
-char buffer_2[(MAX_CHAR*MAX_LINES)];
-// Number of items in the buffer
+// Buffer 2, shared resource between line separator thread and plus sign thread:
+char buffer_2[(50000)];
+// Number of items in the buffer:
 int count_2 = 0;
-// Index where the plus sign thread will put the next item
+// Index where the plus sign thread will put the next item:
 int produced_idx_2 = 0;
-// Index where the plus sign thread will pick up the next item
+// Index where the plus sign thread will pick up the next item:
 int consumed_idx_2 = 0;
-// Initialize the mutex for buffer 2
+// Initialize the mutex for buffer 2:
 pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
-// Initialize the condition variable for buffer 2
+// Initialize the condition variable for buffer 2:
 pthread_cond_t not_empty_2 = PTHREAD_COND_INITIALIZER;
 
+// Flag:
 int buff_3_closed = 0;
-// Buffer 3, shared resource between plus sign thread and output thread
-char buffer_3[(MAX_CHAR*MAX_LINES)];
-// Number of items in the buffer
+// Buffer 3, shared resource between plus sign thread and output thread:
+char buffer_3[50000];
+// Number of items in the buffer:
 int count_3 = 0;
-// Index where the plus sign thread will put the next item
+// Index where the plus sign thread will put the next item:
 int produced_idx_3 = 0;
-// Index where the plus sign thread will pick up the next item
+// Index where the plus sign thread will pick up the next item:
 int consumed_idx_3 = 0;
-// Initialize the mutex for buffer 2
+// Initialize the mutex for buffer 2:
 pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
-// Initialize the condition variable for buffer 2
+// Initialize the condition variable for buffer 2:
 pthread_cond_t not_empty_3 = PTHREAD_COND_INITIALIZER;
 
 /*
  Put an item in buffer_1
 */
-void put_buff_1(char item){
-  // Lock the mutex before putting the item in the buffer
+void buff_1_PRODUCE(char item)
+{
+  // Lock the mutex before putting the item in the buffer:
   pthread_mutex_lock(&mutex_1);
-  // Put the item in the buffer
+  // Put the item in the buffer:
   buffer_1[produced_idx_1] = item;
-  // Increment the index where the next item will be put.
-  produced_idx_1 = produced_idx_1 + 1;
+  // Increment the index where the next item will be put.:
+  produced_idx_1++;
   count_1++;
-  // Signal to the consumer that the buffer is no longer empty
+  // Signal to the consumer that the buffer is no longer empty:
   pthread_cond_signal(&not_empty_1);
-  // Unlock the mutex
+  // Unlock the mutex:
   pthread_mutex_unlock(&mutex_1);
 }
 
 /*
- Function that reads a section of
- the input from the user and returns
- true if "STOP\n" is next, false otherwise.
+ Function that reads user input and returns
+ 1(True) if equal to "STOP/n" returns 0(False) otherwise.
 */
 int stop_processing(char letters[])
 {
@@ -131,32 +134,23 @@ int stop_processing(char letters[])
 */
 void *get_input(void *args)
 {   
+    // While loop keeps the function running:
     while(1) 
     {
-        int j = 0;
-        while (j < (MAX_CHAR*MAX_LINES))
+        // Get the user input:
+        char character[50000];
+        fgets(character, (MAX_CHAR * MAX_LINES), stdin);
+        printf("\n%s\n", character);
+        // Process each char of user input individually:
+        for (int i = 0; i < strlen(character); i++)
         {
-            // Get the user input
-            
-            char character[MAX_CHAR];
-            fgets(character, MAX_CHAR, stdin);
-            for (int i = 0; i < strlen(character); i++)
-            {
-                if (stop_processing(character))
-                {
-                    keep_processing = 0;
-                    pthread_cond_signal(&not_empty_1);
-                    return NULL;
-                }
-                put_buff_1(character[i]);
-                j++;
-            }
-            if (j >= (MAX_CHAR*MAX_LINES))
+            if (stop_processing(character))
             {
                 keep_processing = 0;
                 pthread_cond_signal(&not_empty_1);
                 return NULL;
             }
+            buff_1_PRODUCE(character[i]);
         }
     }
     return NULL;
@@ -165,44 +159,51 @@ void *get_input(void *args)
 /*
 Get the next item from buffer_1
 */
-char get_buff_1(){
-  // Lock the mutex before checking if the buffer has data
-  pthread_mutex_lock(&mutex_1);
-  while (count_1 == 0) {
-    if (!keep_processing)
+char buff_1_CONSUME()
+{
+    // Lock the mutex before checking if the buffer has data:
+    pthread_mutex_lock(&mutex_1);
+    // Check for empty buffer:
+    while (count_1 == 0)
     {
-        pthread_mutex_unlock(&mutex_1);
-        buff_1_closed = 1;
-        return 0;
+        // Chain reaction to raise stop-process flags:
+        if (!keep_processing)
+        {
+            // Update "closed" flag, signal, and end function:
+            pthread_mutex_unlock(&mutex_1);
+            buff_1_closed = 1;
+            return 0;
+        }
+        // Buffer is empty, wait for the producer to signal that the buffer has data:
+        pthread_cond_wait(&not_empty_1, &mutex_1);
     }
-    // Buffer is empty. Wait for the producer to signal that the buffer has data
-    pthread_cond_wait(&not_empty_1, &mutex_1);
-  }
-  char item = buffer_1[consumed_idx_1];
-  // Increment the index from which the item will be picked up
-  consumed_idx_1 = consumed_idx_1 + 1;
-  count_1--;
-  // Unlock the mutex
-  pthread_mutex_unlock(&mutex_1);
-  // Return the item
-  return item;
+    // Gets item:
+    char item = buffer_1[consumed_idx_1];
+    // Increment the index from which the item will be picked up:
+    consumed_idx_1++;
+    count_1--;
+    // Unlock the mutex:
+    pthread_mutex_unlock(&mutex_1);
+    // Returns item:
+    return item;
 }
 
 /*
  Put an item in buffer_2
 */
-void put_buff_2(char item){
-  // Lock the mutex before putting the item in the buffer
-  pthread_mutex_lock(&mutex_2);
-  // Put the item in the buffer
-  buffer_2[produced_idx_2] = item;
-  // Increment the index where the next item will be put.
-  produced_idx_2 = produced_idx_2 + 1;
-  count_2++;
-  // Signal to the consumer that the buffer is no longer empty
-  pthread_cond_signal(&not_empty_2);
-  // Unlock the mutex
-  pthread_mutex_unlock(&mutex_2);
+void buff_2_PRODUCE(char item)
+{
+    // Lock the mutex before putting the item in the buffer:
+    pthread_mutex_lock(&mutex_2);
+    // Put the item in the buffer:
+    buffer_2[produced_idx_2] = item;
+    // Increment the index where the next item will be put:
+    produced_idx_2++;
+    count_2++;
+    // Signal to the consumer that the buffer is no longer empty:
+    pthread_cond_signal(&not_empty_2);
+    // Unlock the mutex:
+    pthread_mutex_unlock(&mutex_2);
 }
 
 /*
@@ -210,29 +211,32 @@ void put_buff_2(char item){
  Consume an item from buffer_1 shared with the input thread.
  Replace any newline character with a space.
  Produce an item in buffer_2 shared with the plus sign thread.
-
 */
 void *separate_line(void *args)
 {
-    
     char item[1];
     char space = ' ';
+    // Process each char individually:
     for (int i = 0; i < (MAX_CHAR*MAX_LINES); i++)
     {
-        item[0] = get_buff_1();
+        item[0] = buff_1_CONSUME();
+        // Check for empty buffer and "closed" flag:
         if (buff_1_closed)
         {
+            // Update "closed" flag, signal, and end function:
             buff_2_closed = 1;
             pthread_cond_signal(&not_empty_2);
             return NULL;
         }
+        // Replaces newline character with a space:
         if (item[0] == '\n')
         {
-            put_buff_2(space);
+            buff_2_PRODUCE(space);
         }
+        // Adds new char to buffer_2:
         else 
         {
-            put_buff_2(item[0]);
+            buff_2_PRODUCE(item[0]);
         }
     }
     return NULL;
@@ -241,48 +245,55 @@ void *separate_line(void *args)
 /*
 Get the next item from buffer_2
 */
-char get_buff_2(){
-  // Lock the mutex before checking if the buffer has data
-  pthread_mutex_lock(&mutex_2);
-  while (count_2 == 0) {
-        if (buff_2_closed == 1)
+char buff_2_CONSUME()
+{
+    // Lock the mutex before checking if the buffer has data:
+    pthread_mutex_lock(&mutex_2);
+    // Check for empty buffer:
+    while (count_2 == 0)
+    {
+        // Chain reaction to raise stop-process flags:
+        if (buff_2_closed)
         {
+            // Update "closed" flag, signal, and end function:
             buff_3_closed = 1;
             pthread_mutex_unlock(&mutex_2);
             return 0;
         }
-    // Buffer is empty. Wait for the producer to signal that the buffer has data
-    pthread_cond_wait(&not_empty_2, &mutex_2);
-  }
-  char item = buffer_2[consumed_idx_2];
-  // Increment the index from which the item will be picked up
-  consumed_idx_2 = consumed_idx_2 + 1;
-  count_2--;
-  // Unlock the mutex
-  pthread_mutex_unlock(&mutex_2);
-  // Return the item
-  return item;
+        // Buffer is empty. Wait for the producer to signal that the buffer has data:
+        pthread_cond_wait(&not_empty_2, &mutex_2);
+    }
+    // Gets item:
+    char item = buffer_2[consumed_idx_2];
+    // Increment the index from which the item will be picked up:
+    consumed_idx_2++;
+    count_2--;
+    // Unlock the mutex:
+    pthread_mutex_unlock(&mutex_2);
+    // Return the item:
+    return item;
 }
 
 /*
  Put an item in buffer_3
 */
-void put_buff_3(char item){
-  // Lock the mutex before putting the item in the buffer
-  pthread_mutex_lock(&mutex_3);
-  // Put the item in the buffer
-  buffer_3[produced_idx_3] = item;
-  // Increment the index where the next item will be put.
-  produced_idx_3 = produced_idx_3 + 1;
-  count_3++;
-  // Signal to the consumer that the buffer is no longer empty
-  pthread_cond_signal(&not_empty_3);
-  // Unlock the mutex
-  pthread_mutex_unlock(&mutex_3);
+void buff_3_PRODUCE(char item)
+{
+    // Lock the mutex before putting the item in the buffer:
+    pthread_mutex_lock(&mutex_3);
+    // Put the item in the buffer:
+    buffer_3[produced_idx_3] = item;
+    // Increment the index where the next item will be put:
+    produced_idx_3++;
+    count_3++;
+    // Signal to the consumer that the buffer is no longer empty:
+    pthread_cond_signal(&not_empty_3);
+    // Unlock the mutex:
+    pthread_mutex_unlock(&mutex_3);
 }
 
 /*
- Helper fucntion for plusChange()
+ Helper function for plusChange()
  that checks the next character present in buffer_2 
  and returns true if it is a "+", false if not.
 */
@@ -291,7 +302,8 @@ int check_next()
     
     // Lock the mutex before checking if the next is a plus.
     pthread_mutex_lock(&mutex_2);
-    if (buffer_2[consumed_idx_2] == '+'){
+    if (buffer_2[consumed_idx_2] == '+')
+    {
         // Unlock the mutex
         pthread_mutex_unlock(&mutex_2);
         return 1;
@@ -301,39 +313,49 @@ int check_next()
     return 0;
 }
 
-
 /*
  Function that the plus sign thread will run. 
- Consume an item from buffer_2 shared with the line separator thread.
+ Consume an item from buffer_2 (shared with the line separator thread)
  Replace any consecutive plusses (++) with a single "^".
  Produce an item in buffer_3 shared with the output thread.
 */
 void *plusChange(void *args)
 {
     char item[1];
-    char exp = '^';
+    char carat = '^';
+    // Process each char individually:
     for (int i = 0; i < (MAX_CHAR*MAX_LINES); i++)
     {
-        item[0] = get_buff_2();
+        item[0] = buff_2_CONSUME();
+        // Check for empty buffer and "closed" flag:
         if (buff_3_closed)
         {
+            // Update "closed" flag, signal, and end function:
             terminate = 1;
             pthread_cond_signal(&not_empty_3);
             return NULL;
         }
+        // Replaces "++" with a carat:
         if (item[0] == '+')
         {
-            if (check_next()){
-                get_buff_2();
-                put_buff_3(exp);
+            // Checks if the next character in the buffer is a '+':
+            if (check_next())
+            {
+                // Calls buff_2_CONSUME() to increment its index and decrement its count:
+                buff_2_CONSUME();
+                // Puts carat in buffer_3:
+                buff_3_PRODUCE(carat);
             }
-            else{
-                put_buff_3(item[0]);
+            // Adds new character to buffer_3:
+            else
+            {
+                buff_3_PRODUCE(item[0]);
             }
         }
+        // Adds new character to buffer_3:
         else
         {
-            put_buff_3(item[0]);
+            buff_3_PRODUCE(item[0]);
         }
     }
     return NULL;
@@ -342,26 +364,30 @@ void *plusChange(void *args)
 /*
 Get the next item from buffer_3
 */
-char get_buff_3(){
-  // Lock the mutex before checking if the buffer has data
-  pthread_mutex_lock(&mutex_3);
-  while (count_3 == 0) {
-      if (terminate == 1)
-      {
-          pthread_mutex_unlock(&mutex_3);
-          return 0;
-      }
-    // Buffer is empty. Wait for the producer to signal that the buffer has data
-    pthread_cond_wait(&not_empty_3, &mutex_3);
-  }
-  char item = buffer_3[consumed_idx_3];
-  // Increment the index from which the item will be picked up
-  consumed_idx_3 = consumed_idx_3 + 1;
-  count_3--;
-  // Unlock the mutex
-  pthread_mutex_unlock(&mutex_3);
-  // Return the item
-  return item;
+char buff_3_CONSUME()
+{
+    // Lock the mutex before checking if the buffer has data
+    pthread_mutex_lock(&mutex_3);
+    while (count_3 == 0)
+    {
+        // Checks if terminate flag is set:
+        if (terminate)
+        {
+            pthread_mutex_unlock(&mutex_3);
+            return 0;
+        }
+        // Buffer is empty. Wait for the producer to signal that the buffer has data
+        pthread_cond_wait(&not_empty_3, &mutex_3);
+    }
+    // Gets item:
+    char item = buffer_3[consumed_idx_3];
+    // Increment the index from which the item will be picked up
+    consumed_idx_3++;
+    count_3--;
+    // Unlock the mutex
+    pthread_mutex_unlock(&mutex_3);
+    // Return the item
+    return item;
 }
 
 /*
@@ -373,34 +399,45 @@ void *write_output(void *args)
 {
     int lines = 0;
     char line_eighty[81];
+    // Process each char individually:
     for (int i = 0; i < (MAX_CHAR*MAX_LINES); i++)
     {
         if (i < 80)
         {
-            line_eighty[i] = get_buff_3();
+            // Adds char to line cadidate:
+            line_eighty[i] = buff_3_CONSUME();
+            // Checks for Terminate flag:
             if (terminate)
             {
                 return NULL;
             }
+            // Prints line if it contains 80 characters:
             if ((i + 1) == 80){
                 printf("%s\n", line_eighty);
+                // Increments lines to track when 50 have been printed:
                 lines++;
             }
         }
         if (i >= 80){
-            line_eighty[(i % 80)] = get_buff_3();
+            // Adds char to line cadidate:
+            line_eighty[(i % 80)] = buff_3_CONSUME();
+            // Checks for Terminate flag:
             if (terminate)
             {
                 return NULL;
             }
+            // Prints line if it contains 80 new characters:
             if ((i + 1) % 80 == 0)
             {
                 printf("%s\n", line_eighty);
+                // Increments lines to track when 50 have been printed:
                 lines++;
             }
         }
+        // Checks if line maximum has been reached:
         if (lines >= MAX_LINES)
         {
+            // Exits entire process since no more lines can be printed:
             exit(3);
             return NULL;
         }
